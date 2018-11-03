e956e89b760ac83e4a1d9fc105964ea894fc66e4
Rocher1512 <44707197+Rocher1512@users.noreply.github.com>
2018-11-03 17:06:08 +0000

    Add files via upload

---
+++ Blow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blow : MonoBehaviour
+{
+    int blowpoint = 1;
+    public float Speed;
+    public int Maxforward;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Blow")
+        {
+            blowpoint += 1;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (blowpoint > 0 && Input.GetKey(KeyCode.W) && (transform.position.z < Maxforward))
+        {
+            transform.Translate(Vector3.left * Speed);
+            blowpoint -= 1;
+        }
+    }
+
+
+}
---
+++ Destroy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour {
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+}
