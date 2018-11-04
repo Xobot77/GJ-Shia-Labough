bb775f81ba747989b1d0732ddbd675d0f966686d
Rocher1512 <44707197+Rocher1512@users.noreply.github.com>
2018-11-04 13:06:00 +0000

    Add files via upload

---
+++ EnterToContinue.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnterToContinue : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+		if(Input.GetKeyDown("return"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+	}
+}
---
+++ MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Instructions()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame ()
+    {
+        Application.Quit();
+    }
+	
+}
